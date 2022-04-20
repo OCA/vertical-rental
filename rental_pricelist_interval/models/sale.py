fb7604a4519d8ddfcc012cfef8fc018231d775d5
cpatel <cpatel@elegosoft.com>
2022-04-20 10:12:47 +0200

    [IMP] rental_pricelist_interval: black, isort, prettier

--- rental_pricelist_interval/models/sale.py
+++ rental_pricelist_interval/models/sale.py
@@ -1,8 +1,7 @@
 # Part of rental-vertical See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models, exceptions, _
-from odoo.tools import float_compare
-from odoo.exceptions import ValidationError, UserError
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
 
 
 class SaleOrderLine(models.Model):
