f8c41c5659e4282f9cd98116d24d9be4aa8ed111
cpatel <cpatel@elegosoft.com>
2022-04-20 10:21:14 +0200

    [MIG] rental_pricelist_interval: Migration to 14.0

--- rental_pricelist_interval/tests/test_rental_pricelist.py
+++ rental_pricelist_interval/tests/test_rental_pricelist.py
@@ -161,4 +161,4 @@ class TestRentalPricelist(RentalStockCommon):
         self.assertEqual(line.price_unit, 1750)
         with self.assertRaises(exceptions.UserError) as e:
             _run_sol_onchange_date(line, end_date=self.date_24_day_later)
-        self.assertEqual("Max rental interval (21 days) is exceeded.", e.exception.name)
+        self.assertEqual("Max rental interval (21 days) is exceeded.", str(e.exception))
