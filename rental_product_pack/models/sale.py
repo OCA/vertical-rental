823360eee0f994544c5076205fd5279453a5d392
Yu Weng <yweng@elegosoft.com>
2022-05-19 18:24:10 +0200

    [IMP] recursive function _create_pack_products (issue #4670)

--- rental_product_pack/models/sale.py
+++ rental_product_pack/models/sale.py
@@ -6,6 +6,7 @@ from odoo import models, api
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
+
     @api.multi
     def action_confirm(self):
         res = super(SaleOrder, self).action_confirm()
@@ -18,17 +19,7 @@ class SaleOrder(models.Model):
             )
             for picking in out_pickings:
                 for move in picking.move_ids_without_package:
-                    if move.product_id and move.product_id.pack_ok:
-                        for line in move.product_id.pack_line_ids:
-                            qty = move.product_uom_qty * line.quantity
-                            new_move = move.copy(
-                                {
-                                    "product_id": line.product_id.id,
-                                    "product_uom_qty": qty,
-                                    "rental_pack_move_id": move.id,
-                                    "picking_id": move.picking_id.id,
-                                }
-                            )
+                    move._create_pack_products()
             out_pickings.action_confirm()
             in_pickings.action_confirm()
         return res
--- rental_product_pack/models/stock.py
+++ rental_product_pack/models/stock.py
@@ -1,6 +1,6 @@
 # Part of rental-vertical See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class StockMove(models.Model):
@@ -10,3 +10,21 @@ class StockMove(models.Model):
         string="Rental Main Pack Move",
         comodel_name="stock.move",
     )
+
+    @api.multi
+    def _create_pack_products(self):
+        self.ensure_one()
+        if self.product_id and not self.product_id.pack_ok:
+            return
+        else:
+            for line in self.product_id.pack_line_ids:
+               qty = self.product_uom_qty * line.quantity
+               new_move = self.copy(
+                   {
+                       "product_id": line.product_id.id,
+                       "product_uom_qty": qty,
+                       "rental_pack_move_id": self.id,
+                       "picking_id": self.picking_id.id,
+                   }
+               )
+               new_move._create_pack_products()
