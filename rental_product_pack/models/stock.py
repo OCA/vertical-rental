8c63eae7f34154714731e33a235c78a7e64659cb
Yu Weng <yweng@elegosoft.com>
2022-06-08 21:12:47 +0200

    [IMP] Merge products from Packs in Rental Picking Out

--- rental_product_pack/models/stock.py
+++ rental_product_pack/models/stock.py
@@ -6,6 +6,7 @@ from odoo import api, fields, models
 class StockMove(models.Model):
     _inherit = "stock.move"
 
+    #TODO Delete field rental_pack_move_id
     rental_pack_move_id = fields.Many2one(
         string="Rental Main Pack Move",
         comodel_name="stock.move",
@@ -19,12 +20,21 @@ class StockMove(models.Model):
         else:
             for line in self.product_id.pack_line_ids:
                qty = self.product_uom_qty * line.quantity
-               new_move = self.copy(
-                   {
-                       "product_id": line.product_id.id,
-                       "product_uom_qty": qty,
-                       "rental_pack_move_id": self.id,
-                       "picking_id": self.picking_id.id,
-                   }
+               move = self.search(
+                   [
+                       ('picking_id', '=', self.picking_id.id),
+                       ('product_id', '=', line.product_id.id),
+                   ]
                )
-               new_move._create_pack_products()
+               if move and not line.product_id.pack_ok:
+                   move.product_uom_qty += qty
+               else:
+                   new_move = self.copy(
+                       {
+                           "product_id": line.product_id.id,
+                           "product_uom_qty": qty,
+                           "rental_pack_move_id": self.id,
+                           "picking_id": self.picking_id.id,
+                       }
+                   )
+                   new_move._create_pack_products()
