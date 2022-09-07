d951ed6dcc601337a30d300668e2da8dd9464756
Yu Weng <yweng@elegosoft.com>
2022-09-07 10:50:01 +0200

    [IMP] rental_check_availability: black, isort, prettier

--- rental_check_availability/__manifest__.py
+++ rental_check_availability/__manifest__.py
@@ -2,16 +2,21 @@
 
 {
     "name": "Rental Check Availability",
-    "summary": "Extends the sale_rental module for checking availability of the rented product.",
+    "summary": """Extends the sale_rental module for checking availability
+of the rented product.
+""",
     "description": """
-This module activates availability checks on stockable products related to rental services in
-sale orders. In the base functionality only the total amount of products in stock is checked and user is
-informed when the amount of products to rent out in a sale order is higher.
+This module activates availability checks on stockable products related to
+rental services in sale orders. In the base functionality only the total
+amount of products in stock is checked and user is informed when the amount
+of products to rent out in a sale order is higher.
 
-After the installation of this module the availability is checked in consideration of the total amount
-of goods in stock and the amount of products used in concurrent sale orders at the certain desired timeframe.
-In case of insufficient products in stock the user receives visual notification on respective sale order line
-and can access the list of concurrent sale orders directly.
+After the installation of this module the availability is checked in
+consideration of the total amount of goods in stock and the amount of
+products used in concurrent sale orders at the certain desired timeframe.
+In case of insufficient products in stock the user receives visual
+notification on respective sale order line and can access the list of
+concurrent sale orders directly.
 """,
     "usage": """
 To use this module, you need to:
@@ -35,6 +40,7 @@ To use this module, you need to:
     "version": "12.0.1.0.0",
     "category": "Rental",
     "author": "Odoo Community Association (OCA)/Elego Software Solutions GmbH",
+    "website": "https://github.com/OCA/vertical-rental",
     "depends": [
         "rental_pricelist",
     ],
