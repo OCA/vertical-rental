a061191b92e48254bee8dfa501ad0ae3c3177e44
Yu Weng <yweng@elegosoft.com>
2022-09-07 11:36:35 +0200

    [IMP] delete description in manifest of module rental_offday

--- rental_offday/__manifest__.py
+++ rental_offday/__manifest__.py
@@ -5,15 +5,6 @@
     "version": "12.0.1.0.0",
     "category": "Rental",
     "summary": "Manage off-days in rentals on daily basis",
-    "description": """
-During short-term rentals over several days or weeks, the customer and the salesman
-agree on so called off-days. On these days the customer still have the rented products
-but usually doesn't use them and, therefore, does not pay the daily price. This is often
-the case for weekends and holidays, since there might be some legal limitations in using
-the products on these days.
-In order to meet this requirement, the salesman can add off-days on sale order lines for
-products that are rentable in days. These days will not be included in price calculation.
-""",
     "usage": """
 The off-days can only be used for products rentable in days.
 
