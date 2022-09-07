abe186a66554e15ce5deac4bc4a442e00a43205b
Yu Weng <yweng@elegosoft.com>
2022-09-07 10:50:18 +0200

    [IMP] rental_offday: black, isort, prettier

--- rental_offday/__manifest__.py
+++ rental_offday/__manifest__.py
@@ -37,9 +37,11 @@ Create a rental order:
  * Set the quantity to rent out one or several storable rentable products.
  * Choose start and end date, e.g. for 3 weeks.
  * On the order line you will see a page 'Off-Days' at the bottom.
- * Choose the type 'Weekend' in order to create 'Fixed Off-Days' and you get a list with all saturdays and sundays within the rental period.
+ * Choose the type 'Weekend' in order to create 'Fixed Off-Days'
+   and you get a list with all saturdays and sundays within the rental period.
  * Add some additional off-days as needed.
- * The number of off-days reduces the rental quantity and is therefore not included in price calculation.
+ * The number of off-days reduces the rental quantity
+   and is therefore not included in price calculation.
  * Confirm the order.
 """,
     "author": "elego Software Solutions GmbH, Odoo Community Association (OCA)",
