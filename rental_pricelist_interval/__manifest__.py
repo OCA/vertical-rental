ffa3844460d71e38d7741bc49d8e372e70fcbd24
pwitte <pwitte@elegosoft.com>
2024-02-19 14:10:49 +0100

    [MIG] rental_pricelist_interval: Migration to 15.0

--- rental_pricelist_interval/__manifest__.py
+++ rental_pricelist_interval/__manifest__.py
@@ -8,6 +8,17 @@
     "category": "Rental",
     "author": "elego Software Solutions GmbH, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/vertical-rental",
+    "configuration": """
+To configure this module, you need to:
+
+#. Go to company settings and define the default interval ranges on
+   'Rental Interval Prices' tab. These ranges will be applied for
+   computation of price intervals for rental service products
+   when interval pricing is activated in stockable product.
+
+#. If desired go to 'RS (Prefix and Suffix)' tab an define how rental interval service product
+   names and reference numbers are created.
+""",
     "contributors": [
         "Ben Brich <b.brich@humanilog.org> (www.humanilog.org)",
         "Yu Weng <yweng@elegosoft.com> (www.elegosoft.com)",
