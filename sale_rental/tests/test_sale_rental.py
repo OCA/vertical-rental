e415f04ab05354d075834cc1ba0a2b64b90199c4
pwitte <pwitte@elegosoft.com>
2024-03-14 14:54:34 +0100

    [FIX] rework unit test

--- sale_rental/tests/test_sale_rental.py
+++ sale_rental/tests/test_sale_rental.py
@@ -2,7 +2,6 @@ from odoo.exceptions import UserError
 from odoo.tests import Form
 from odoo.tests.common import TransactionCase
 
-
 class TestSaleRental(TransactionCase):
     @classmethod
     def setUpClass(cls):
@@ -21,18 +20,18 @@ class TestSaleRental(TransactionCase):
             "start_date": "2022-01-01",
             "end_date": "2022-01-10",
             "rental_qty": 1,
+            "rental": True,
+            "rental_type": "new_rental",
+            "product_uom_qty": 10,
+            "number_of_days": 10
         }
-
-        # with so_form.order_line.new() as line:
-        #     line.product_id = self.test_rental_prod
-        # line.start_date = "2022-01-01"
-        # line.end_date = "2022-01-10"
-        # line.rental_qty = 1
         so = so_form.save()
         so.write({"order_line": [(0, 0, line_vals)]})
-        sol = so.order_line
-        self.assertEqual(sol.price_subtotal, 60)
+        sol = so.order_line[0]
+        self.assertEqual(sol.price_subtotal, 600)
         so.action_confirm()
+
+
         self.assertEqual(len(so.picking_ids), 2)
         rental_out_pick = so.picking_ids.filtered(
             lambda p: p.location_id == self.rental_in_loc
@@ -54,24 +53,23 @@ class TestSaleRental(TransactionCase):
             line.product_id = self.test_rental_prod.rented_product_id
             line.product_uom_qty = 1
             line.sell_rental_id = rental
-        so = so_form.save()
+        so2 = so_form.save()
         line_vals = {
             "product_id": self.test_rental_prod.rented_product_id.id,
             "name": "Test",
             "display_type": False,
-            "product_uom": self.env.ref("uom.product_uom_day").id,
             "product_uom_qty": 1,
-            "sell_rental_id": rental,
+            "sell_rental_id": rental.id,
         }
-        so.write({"order_line": [(0, 0, line_vals)]})
-        sol = so.order_line
+        so2.write({"order_line": [(0, 0, line_vals)]})
+        sol = so2.order_line
         # Raises an error because rented product is not sent yet
         with self.assertRaises(UserError):
-            so.action_confirm()
+            so2.action_confirm()
         # Confirm the rental delivery and check the return which should
         # be cancelled
         rental_out_pick.action_assign()
         rental_out_pick.action_set_quantities_to_reservation()
         rental_out_pick.button_validate()
-        so.action_confirm()
+        so2.action_confirm()
         self.assertEqual(rental_in_pick.state, "cancel")
