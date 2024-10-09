3f959b98da48bb56a39008829bb32de5fe73ec05
Yu Weng <yweng@elegosoft.com>
2024-10-09 14:12:48 +0200

    [MIG] rental_pricelist_interval: porting of missing changes after migration from v12 to v14

--- rental_pricelist_interval/__manifest__.py
+++ rental_pricelist_interval/__manifest__.py
@@ -2,16 +2,43 @@
 
 {
     "name": "Rental Pricelist (Interval)",
-    "summary": "Enables the user to define different rental prices "
-    "time uom (Month, Day and Hour).",
+    "summary": """
+Enables the user to define different rental prices with time uom ("Month", "Day" and "Hour").
+""",
+    "usage": """
+To use this module, you need to:
+
+#. Create a new stockable product and define it as rental service or
+   go to an existing one.
+
+#. On 'Rental Price' tab check the 'Rented in interval' option.
+
+#. Set the interval base price and define the max amount of days the product
+   can be rented out.
+
+#. Push the 'Reset Interval Prices' button to compute interval ranges and prices.
+   from base price and interval ranges configured in company settings.
+
+#. Adapt interval min. quantities or prices for the selected product if desired.
+""",
+    "contributors": """
+* Ben Brich <b.brich@humanilog.org> (www.humanilog.org)
+* Yu Weng <yweng@elegosoft.com> (www.elegosoft.com)
+""",
+    "configuration": """
+To configure this module, you need to:
+
+#. Go to company settings and define the default interval ranges on
+   'Rental Interval Prices' tab. These ranges will be applied for computation of price intervals
+   for rental service products when interval pricing is activated in stockable product.
+
+#. If desired go to 'RS (Prefix and Suffix)' tab an define how rental interval service product
+   names and reference numbers are created.
+""",
     "version": "14.0.1.0.0",
     "category": "Rental",
     "author": "elego Software Solutions GmbH, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/vertical-rental",
-    "contributors": [
-        "Ben Brich <b.brich@humanilog.org> (www.humanilog.org)",
-        "Yu Weng <yweng@elegosoft.com> (www.elegosoft.com)",
-    ],
     "depends": [
         "rental_pricelist",
     ],
